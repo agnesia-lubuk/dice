a40c6bcd30f719d5f22399b7de241ffc1f3dffaa
Aggy <56287597+agnesia-lubuk@users.noreply.github.com>
2020-01-24 10:34:03 +0700

    refresh me alert

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+alert ("Refresh Me!");
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 var randomImageSource = "images/" + randomDiceImage ;
